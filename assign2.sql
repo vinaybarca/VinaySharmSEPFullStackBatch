103210c460c367996792a99a7d5a5598c69d9f87
Vinay Sharma <vinaybarca22@gmail.com>
2021-07-15 14:48:50 -0400

    Added correct answers from answer sheet

--- assign2.sql
+++ assign2.sql
@@ -1,5 +1,5 @@
-USE AdventureWorks2019
-go
+--USE AdventureWorks2019
+--go
 
 --Part 2
 
@@ -54,3 +54,158 @@ go
 
 --Question 13
 --SELECT DISTINCT Person.CountryRegion.Name AS 'Country', Person.StateProvince.Name AS 'Province' from Person.CountryRegion   JOIN Person.StateProvince ON Person.CountryRegion.CountryRegionCode = Person.StateProvince.CountryRegionCode WHERE Person.CountryRegion.name IN ('Germany', 'Canada') AND Person.StateProvince.Name IS NOT NULL GROUP BY Person.CountryRegion.Name, Person.StateProvince.Name ORDER BY Person.CountryRegion.Name asc
+
+
+
+
+
+USE Northwind
+go
+
+
+--Question 14
+--SELECT dbo.[Order Details].productID , dbo.orders.OrderDate,Quantity from dbo.[Order Details] FULL JOIN dbo.orders ON dbo.orders.OrderID = dbo.[Order Details].OrderID where DATEDIFF(year, dbo.Orders.OrderDate, GETDATE()) < 1   order by dbo.orders.OrderDate DESC
+
+
+--Question 15
+SELECT TOP 5 ShipPostalCode,  SUM([Order Details].Quantity) from Orders FULL JOIN [Order Details] ON orders.OrderID = [Order Details].OrderID WHERE Orders.ShipPostalCode IS NOT NULL GROUP BY ShipPostalCode 
+
+
+
+SELECT TOP 5 o.ShipPostalCode, SUM(od.Quantity) as qty FROM 
+	Orders o
+	FULL JOIN
+	[Order Details] od
+	ON o.OrderID =  od.OrderID
+	WHERE o.ShipPostalCode IS NOT NULL 
+		AND DATEDIFF(year, o.OrderDate, GETDATE())< 21
+	GROUP BY ShipPostalCode
+	ORDER BY qty DESC;
+
+
+select count(customerID),City
+from customers
+group by City
+
+
+select count(customerID),City
+from customers
+group by City
+having  count(customerID)>10
+
+
+
+
+SELECT DISTINCT c.CustomerID, c.CompanyName, c.ContactName FROM 
+	Orders o
+	INNER JOIN 
+	Customers c
+	ON o.CustomerID = c.CustomerID
+	WHERE OrderDate > '1998-1-1';
+
+
+SELECT c.CustomerID, c.CompanyName, c.ContactName FROM
+	Orders o
+	INNER JOIN
+	Customers c
+	ON o.CustomerID = c.CustomerID
+	WHERE o.OrderDate IN 
+	(SELECT TOP 1 OrderDate FROM Orders
+	WHERE OrderDate IS NOT NULL
+	GROUP BY OrderDate
+	ORDER BY OrderDate DESC);
+
+SELECT c.CustomerID, c.CompanyName, c.ContactName, 
+SUM(od.Quantity) AS QTY FROM 
+Customers c 
+LEFT JOIN 
+Orders o 
+ON c.CustomerID = o.CustomerID
+LEFT JOIN 
+[Order Details] od
+ON o.OrderID = od.OrderID
+GROUP BY c.CustomerID, c.CompanyName, c.ContactName
+ORDER BY QTY;
+
+
+SELECT c.CustomerID,
+SUM(od.Quantity) AS QTY FROM 
+Customers c 
+LEFT JOIN 
+Orders o 
+ON c.CustomerID = o.CustomerID
+LEFT JOIN 
+[Order Details] od
+ON o.OrderID = od.OrderID
+GROUP BY c.CustomerID
+HAVING SUM(od.Quantity) > 100
+ORDER BY QTY;
+
+
+SELECT DISTINCT sup.CompanyName, ship.CompanyName FROM 
+Orders o
+LEFT JOIN
+[Order Details] od
+ON o.OrderID = od.OrderID
+INNER JOIN 
+Products p
+ON od.ProductID = p.ProductID
+RIGHT JOIN
+Suppliers sup
+ON p.SupplierID = sup.SupplierID
+INNER JOIN
+Shippers ship
+ON o.ShipVia = ship.ShipperID;
+
+SELECT o.OrderDate, p.ProductName FROM 
+Orders o
+LEFT JOIN
+[Order Details] od
+ON o.OrderID = od.OrderID
+INNER JOIN
+Products p
+ON od.ProductID = p.ProductID
+GROUP BY o.OrderDate, p.ProductName
+ORDER BY o.OrderDate;
+
+
+SELECT Title, LastName + ' ' + FirstName AS Name 
+FROM Employees
+ORDER BY Title;
+
+
+SELECT e1.Title, e1.LastName + ' ' + e1.FirstName AS Name1, e2.LastName + ' ' + e2.FirstName AS Name2 
+FROM Employees e1
+JOIN 
+Employees e2
+ON e1.Title = e2.Title 
+WHERE e1.FirstName <> e2.FirstName OR e1.LastName <>        e2.LastName
+ORDER BY Title;
+
+SELECT * FROM (SELECT * FROM Employees) AS T1
+INNER JOIN
+(SELECT ReportsTo, COUNT(ReportsTo) AS Subordinate  FROM Employees
+WHERE ReportsTo IS NOT NULL
+GROUP BY ReportsTo
+HAVING COUNT(ReportsTo) > 2) T2
+ON T2.ReportsTo= T1.EmployeeID;
+Question: How to keep most columns but drop only a few of them?
+
+
+SELECT c.City, c.CompanyName, c.ContactName, 'Customer' as Type
+FROM Customers c
+UNION
+SELECT s.City, s.CompanyName, s.ContactName, 'Supplier' as Type
+FROM Suppliers s;
+
+
+SELECT * FROM F1 INNER JOIN F2;
+ON F1.T1 = F2.T2
+
+
+SELECT * FROM F1 LEFT JOIN F2;
+ON F1.T1 = F2.T
+
+SELECT * FROM F1 FULL JOIN F2;
+ON F1.T1 = F2.T2
+
